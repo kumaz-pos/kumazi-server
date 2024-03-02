e70d3f6a228598c3436e5bea2097be27470e9ce8
Tawonga021 <reginaldjongwe@gmail.com>
2024-03-02 06:44:36 -0800

    updated cors

--- server.js
+++ server.js
@@ -11,19 +11,18 @@ const shopRouter = require('./routes/shops');
 const branchRouter = require('./routes/branches');
 const storeKeeperRouter=require("./routes/store-keepers")
 dotenv.config();
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
-    );
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
-  });
-app.use(cors())
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use('/api/v1/',authRouter);
 app.use('/api/v1/',salesRouter);
