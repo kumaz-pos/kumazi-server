ef507b63ddcbba420b49759ea999413863df0d71
Tawonga021 <reginaldjongwe@gmail.com>
2024-03-02 06:32:34 -0800

    updated cors props

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const corsOptions ={
    
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+ });
 app.use(cors(corsOptions))
 app.use(express.json());
 app.use('/api/v1/',authRouter);
