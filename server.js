39b480a1f7b54414fd2e709b5845b6a3b0f4e5a5
Tawonga021 <reginaldjongwe@gmail.com>
2024-03-02 06:22:16 -0800

    updated package.json

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors())
+app.use(cors({ credentials: true }))
 app.use(express.json());
 app.use('/api/v1/',authRouter);
 app.use('/api/v1/',salesRouter);
