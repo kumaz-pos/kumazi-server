b803d74421038ec6737911f1d8cdbd49815335c8
Tawonga021 <reginaldjongwe@gmail.com>
2024-03-02 06:35:44 -0800

    updated cors package

--- server.js
+++ server.js
@@ -11,17 +11,19 @@ const shopRouter = require('./routes/shops');
 const branchRouter = require('./routes/branches');
 const storeKeeperRouter=require("./routes/store-keepers")
 dotenv.config();
-const corsOptions ={
-    AccessControlAllowOrigin: '*',
-   
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
-}
-app.all('*', function(req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
     next();
- });
-app.use(cors(corsOptions))
+  });
+app.use(cors())
 app.use(express.json());
 app.use('/api/v1/',authRouter);
 app.use('/api/v1/',salesRouter);
