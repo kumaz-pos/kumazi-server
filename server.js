6f3ef6be5625a17772682367e76d06236fc16055
Tawonga021 <reginaldjongwe@gmail.com>
2024-03-06 11:59:52 -0800

    added rate limiter

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const shopRouter = require('./routes/shops');
 const branchRouter = require('./routes/branches');
 const storeKeeperRouter=require("./routes/store-keepers")
 const compression= require("compression")
+const {rateLimit}= require("express-rate-limit");
 dotenv.config();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -18,7 +19,13 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
-
+const limiter= rateLimit({
+    windowMs:15*60*1000,
+    limit:100,
+    standardHeaders:"draft-7",
+    legacyHeaders:false
+})
+app.use(limiter)
 app.use(cors({ origin: null , credentials :  true,allowedHeaders:['Content-Type', 'Authorization']}));
 app.use(express.json());
 app.use(compression())
