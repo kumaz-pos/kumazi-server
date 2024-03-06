5a390d9b9d1e29c630cc3fcd9e96061f15aceacf
Tawonga021 <reginaldjongwe@gmail.com>
2024-03-06 11:49:46 -0800

    added compression module

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const productRouter = require('./routes/products');
 const shopRouter = require('./routes/shops');
 const branchRouter = require('./routes/branches');
 const storeKeeperRouter=require("./routes/store-keepers")
+const compression= require("compression")
 dotenv.config();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -20,6 +21,7 @@ app.use(function(req, res, next) {
 
 app.use(cors({ origin: null , credentials :  true,allowedHeaders:['Content-Type', 'Authorization']}));
 app.use(express.json());
+app.use(compression())
 app.use('/api/v1/',authRouter);
 app.use('/api/v1/',salesRouter);
 app.use('/api/v1/',productRouter);
