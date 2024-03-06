41b52ebbb154804642b30525da3e7c518b1751f6
Tawonga021 <reginaldjongwe@gmail.com>
2024-03-06 01:22:15 -0800

    added patch method to the allowed control methods on cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const storeKeeperRouter=require("./routes/store-keepers")
 dotenv.config();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
